d6036822e060d62c56979b573f9a7bebd49673c3
Mark Paschal <markpasc@markpasc.org>
2013-09-05 10:42:13 -0400

    Some boilerplate files

---
+++ README.md
@@ -0,0 +1,3 @@
+# anagrem #
+
+`anagrem` is a tiny Gearman client for Python 3.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+
+setup(
+    name='anagrem',
+    version='1.0',
+    description='a simple Gearman client (in pure Python 3)',
+    packages=['anagrem'],
+
+    author='Mark Paschal',
+    author_email='markpasc@markpasc.org',
+    url='https://github.com/duncaningram/anagrem',
+
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
