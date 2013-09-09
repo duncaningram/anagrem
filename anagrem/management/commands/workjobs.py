5c00cb27d59c9b92bb1a1ca393e3028b4ea6dd1b
Mark Paschal <markpasc@markpasc.org>
2013-09-09 15:46:01 -0400

    Oops, load Django apps before working
    so their jobs are registered by the time we start.

--- anagrem/management/commands/workjobs.py
+++ anagrem/management/commands/workjobs.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+from django.utils.importlib import import_module
 
 import anagrem.models
 
@@ -9,4 +10,12 @@ class Command(BaseCommand):
     help = 'Start a Gearman worker to work jobtasks'
 
     def handle(self, *args, **options):
+        # Load all apps' jobs so they're registered.
+        for app_name in settings.INSTALLED_APPS:
+            try:
+                import_module('.jobs', app_name)
+            except ImportError:
+                pass
+
+        # Do the work now that everything's loaded.
         anagrem.models.client.work()
